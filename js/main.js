dc7a0ad2cbc9bcfba3c847540723b3ea0d03b5b2
Mahad <mamir3012@gmail.com>
2021-09-14 10:53:07 +0500

    Use moment.js for current exp date window calculation

--- js/main.js
+++ js/main.js
@@ -1,10 +1,5 @@
-function monthDiff(d1, d2) {
-    var months;
-    months = (d2.getFullYear() - d1.getFullYear()) * 12;
-    months -= d1.getMonth();
-    months += d2.getMonth();
-    return months <= 0 ? 0 : months;
-}
+const startDate = moment([2020, 3, 1]);
+const years = moment().diff(startDate, 'year');
+const months = moment().diff(startDate.add(years, 'years'), 'months');
 
-// lord forgive me for what i am doing here
-document.getElementById("current-work-exp").innerHTML = `(${monthDiff(new Date(2020, 3, 1), new Date())} months)`;
+document.getElementById("current-work-exp").innerHTML = `(${years} year ${months} months)`;
