ef21e56b5eba8381bb284146ec3b5b507d9c6d30
Mahad <mamir3012@gmail.com>
2021-10-22 12:26:48 +0500

    Update Resume

--- js/main.js
+++ js/main.js
@@ -2,4 +2,6 @@ const startDate = moment([2020, 3, 1]);
 const years = moment().diff(startDate, 'year');
 const months = moment().diff(startDate.add(years, 'years'), 'months');
 
-document.getElementById("current-work-exp").innerHTML = `(${years} year ${months} months)`;
+let currentExpString = `(${years} year${years > 1 ? 's' : ''}`
+currentExpString += months > 0 ? ` ${months} months)`: ')';
+document.getElementById("current-work-exp").innerHTML = currentExpString;
